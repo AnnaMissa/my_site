ff3f69bfe73fb34548cbe909b7c4dc71cfe3c31d
Anna <anni.missa@gmail.com>
2018-07-30 23:14:10 +0300

    'add_first_info_about_shop_products'

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'shop.apps.ShopConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views import generic
+from django.utils import timezone
 
-# Create your views here.
+from .models import Product
+
+
+class ProductListView(generic.ListView):
+    context_object_name = 'new_product_list'
+
+    def get_queryset(self):
+        """
+        Возвращает последние пять созданных товаров
+        :return: 
+        """
+        return Product.objects.filter(
+            create_date__lte=timezone.now()
+        ).order_by('-create_date')[:5]
+
+
+class ProductDetailView(generic.DetailView):
+    model = Product

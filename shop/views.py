07a06c5bc49b8cce57f44db37d0d950bf3ac17ea
Anna <anni.missa@gmail.com>
2018-08-07 22:39:40 +0300

    'view_and_template_peoduct_category'

--- shop/views.py
+++ shop/views.py
@@ -2,21 +2,34 @@ from django.shortcuts import render
 from django.views import generic
 from django.utils import timezone
 
-from .models import Product
+from .models import Product, ProductCategory
 
 
-class ProductListView(generic.ListView):
-    context_object_name = 'new_product_list'
+class ProductCategoryListView(generic.ListView):
+    template_name = 'shop/product_category_list.html'
+    context_object_name = 'product_category_list'
 
     def get_queryset(self):
         """
-        Возвращает последние пять созданных товаров
+        Возвращает существующие категории товаров
+        :return: 
+        """
+        return ProductCategory.objects.order_by('name')
+
+
+class ProductListView(generic.ListView):
+    context_object_name = 'product_list'
+
+    def get_queryset(self):
+        """
+        Возвращает продукты выбранной категории
         :return: 
         """
         return Product.objects.filter(
-            create_date__lte=timezone.now()
-        ).order_by('-create_date')[:5]
+            category=self.kwargs['pk']
+        ).order_by('-create_date')
 
 
 class ProductDetailView(generic.DetailView):
     model = Product
+
